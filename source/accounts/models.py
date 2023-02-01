c49e42ea78304e9489bd0511b0c78076ed66a1c7
ghost <kirill.han011@gmail.com>
2023-02-01 11:59:58 +0300

    Изменено поле номера телефона в моделях accounts. Добавлена кнопка сохранить в update_profile

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import RegexValidator
 from django.db import models
 
 User = get_user_model()
@@ -8,10 +7,7 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name="profile", on_delete=models.CASCADE,
                                 verbose_name='Профиль')
-    phone_number = models.CharField(max_length=17, verbose_name='Номер телефона', validators=[RegexValidator(
-        regex='^\+[9]{2}?[6] [0-9]{3} [0-9]{3} [0-9]{3}$', message='Введите номер телефона в формате +996 ХХХ ХХХ ХХХ'
-    )]
-                                    )
+    phone_number = models.CharField(max_length=17, blank=True, verbose_name="Номер телефона")
     email = models.EmailField(blank=False, null=False, verbose_name="Email")
     first_name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Имя")
     last_name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Фамилия")
